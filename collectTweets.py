c14f64111fe6d43458cd97669c747d92a20ad223
Annalise Ko <annaliseliuko@gmail.com>
2017-11-30 17:28:24 -0800

    added user id, location and timezone

--- collectTweets.py
+++ collectTweets.py
@@ -4,35 +4,6 @@ import time
 import pyspark
 import pyspark.streaming
 
-# # common setup
-# def printItems(items):
-#     for item in items:
-#         print item.encode('utf-8')
-        
-# def main():
-#     sc = pyspark.SparkContext(master="local[%i]" % 2)
-#     sc.setLogLevel("ERROR")
-#     ssc = pyspark.streaming.StreamingContext(sc, 10)
-
-#     socket_stream = ssc.socketTextStream("127.0.0.1", 5555)
-    
-#     lines = socket_stream.window(10)
-#     #lines.pprint()
-    
-#     #hashtags = lines.flatMap(lambda line: line.split(" "))\
-#     #                .filter(lambda word: word.lower().startswith('#'))\
-#     # hashtags.pprint()                   
-
-#     counts = lines.flatMap(lambda line: line.split(" ")) \
-#                   .filter(lambda word: word.lower().startswith("#"))\
-#                   .map(lambda word: (word, 1)) \
-#                   .reduceByKey(lambda a, b: a+b)\
-#                   .transform(lambda rdd: rdd.sortBy(lambda a: a[1],ascending=False))
-#     counts.pprint()
-
-#     ssc.start()
-#     ssc.awaitTermination()
-
 import tweepy
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -41,12 +12,6 @@ import socket
 import json
 import logging
 import csv
-
-#Twitter API credentials
-CONSUMER_KEY = 'tOpKHcNzNpu88PSZxAzCI87Ne'
-CONSUMER_SECRET = 'EhSEN89oydJi058EkQP3iMjsVlYw6yLYZ2Uq2UAVSWS43wXju9'
-ACCESS_TOKEN = '37501551-hUS1bgjvyBq9H1pplnXkQb1rBIqNfNzPsBHFtx8dw'
-ACCESS_SECRET = '0MI19XnM6FXT8D8LWM70KbHYZDp5GefyZpYwD6hUUvtSD'
 
 #Twitter API credentials
 CONSUMER_KEY = 'tOpKHcNzNpu88PSZxAzCI87Ne'
@@ -65,48 +30,26 @@ def get_all_tweets(screen_name):
     api = tweepy.API(auth)
     
     #initialize a list to hold all the tweepy Tweets
-    alltweets = []  
+    all_tweets = []  
     
     #make initial request for most recent tweets (200 is the maximum allowed count)
-    new_tweets = api.user_timeline(screen_name = screen_name,count=5)
+    temp = api.user_timeline(screen_name = screen_name,count=10)
     
     #save most recent tweets
-    alltweets.extend(new_tweets)
+    all_tweets.extend(temp)
     
     #save the id of the oldest tweet less one
-    oldest = alltweets[-1].id - 1
-    
-    #keep grabbing tweets until there are no tweets left to grab
-    # while len(new_tweets) > 0:
-    #     print "getting tweets before %s" % (oldest)
-        
-    #     #all subsiquent requests use the max_id param to prevent duplicates
-    #     new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
-        
-    #     #save most recent tweets
-    #     alltweets.extend(new_tweets)
-        
-    #     #update the id of the oldest tweet less one
-    #     oldest = alltweets[-1].id - 1
-        
-    #     print "...%s tweets downloaded so far" % (len(alltweets))
+    oldest = all_tweets[-1].id - 1
     
     #transform the tweepy tweets into a 2D array that will populate the csv 
-    outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
-    
-    # retweetTemp = []
-    # retweetTemp = api.retweets(tweet.id_str)
+    outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in all_tweets]
 
-    # retweets = []
-    # retweets = [[retweet.id_str, retweet.text.encode("utf-8")] for retweet in retweetTemp]
 
-    
     #write the csv  
     with open('%s_tweets.csv' % screen_name, 'wb') as f:
         writer = csv.writer(f)
         writer.writerow(["id","created_at","text"])
         writer.writerows(outtweets)
-    
     pass
 
 
@@ -114,5 +57,3 @@ if __name__ == '__main__':
     #pass in the username of the account you want to download
     get_all_tweets("BarackObama")
 
-# if __name__ == "__main__":
-#     main()
--- retweets.py
+++ retweets.py
@@ -4,35 +4,6 @@ import time
 import pyspark
 import pyspark.streaming
 
-# # common setup
-# def printItems(items):
-#     for item in items:
-#         print item.encode('utf-8')
-        
-# def main():
-#     sc = pyspark.SparkContext(master="local[%i]" % 2)
-#     sc.setLogLevel("ERROR")
-#     ssc = pyspark.streaming.StreamingContext(sc, 10)
-
-#     socket_stream = ssc.socketTextStream("127.0.0.1", 5555)
-    
-#     lines = socket_stream.window(10)
-#     #lines.pprint()
-    
-#     #hashtags = lines.flatMap(lambda line: line.split(" "))\
-#     #                .filter(lambda word: word.lower().startswith('#'))\
-#     # hashtags.pprint()                   
-
-#     counts = lines.flatMap(lambda line: line.split(" ")) \
-#                   .filter(lambda word: word.lower().startswith("#"))\
-#                   .map(lambda word: (word, 1)) \
-#                   .reduceByKey(lambda a, b: a+b)\
-#                   .transform(lambda rdd: rdd.sortBy(lambda a: a[1],ascending=False))
-#     counts.pprint()
-
-#     ssc.start()
-#     ssc.awaitTermination()
-
 import tweepy
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -49,7 +20,23 @@ CONSUMER_SECRET = 'EhSEN89oydJi058EkQP3iMjsVlYw6yLYZ2Uq2UAVSWS43wXju9'
 ACCESS_TOKEN = '37501551-hUS1bgjvyBq9H1pplnXkQb1rBIqNfNzPsBHFtx8dw'
 ACCESS_SECRET = '0MI19XnM6FXT8D8LWM70KbHYZDp5GefyZpYwD6hUUvtSD'
 
+auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+api = tweepy.API(auth)
+
 # stores all the tweet ids from the csv file into an array, tweet_id
 data = pd.read_csv('BarackObama_tweets.csv')
 tweet_id = data.id
 
+# stores all the retweet ids and original tweet to a new csv file
+with open('retweets.csv', 'wb') as f:
+    writer = csv.writer(f)
+    writer.writerow(["user", "user location", "time zone", "geo enabled", "coordinates", "text"])
+
+    # need to potentially calculate coordinates of tweet???
+    for x in range(len(tweet_id)):
+        retweetTemp = api.retweets(id = tweet_id[x], count = 100)
+        retweets = [[retweet.user.id_str, retweet.user.location.encode("utf-8"), retweet.user.time_zone, retweet.user.geo_enabled, retweet.coordinates, retweet.text.encode("utf-8")] for retweet in retweetTemp]
+        writer.writerows(retweets)
+pass
