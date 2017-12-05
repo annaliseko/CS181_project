d39765361db107249e31c5b720afaab4d0868b67
Patrick McDonough <31291053+pmcdonough35@users.noreply.github.com>
2017-12-05 11:58:43 -0800

    collect max API amount of tweets from user

--- collectTweets.py
+++ collectTweets.py
@@ -30,30 +30,48 @@ def get_all_tweets(screen_name):
     api = tweepy.API(auth)
     
     #initialize a list to hold all the tweepy Tweets
-    all_tweets = []  
+    alltweets = []  
     
     #make initial request for most recent tweets (200 is the maximum allowed count)
-    temp = api.user_timeline(screen_name = screen_name,count=10)
+    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
     
     #save most recent tweets
-    all_tweets.extend(temp)
+    alltweets.extend(new_tweets)
     
     #save the id of the oldest tweet less one
-    oldest = all_tweets[-1].id - 1
+    oldest = alltweets[-1].id - 1
+    
+    #keep grabbing tweets until there are no tweets left to grab
+    while len(new_tweets) > 0:
+        print "getting tweets before %s" % (oldest)
+        
+        #all subsiquent requests use the max_id param to prevent duplicates
+        new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+        
+        #save most recent tweets
+        alltweets.extend(new_tweets)
+        
+        #update the id of the oldest tweet less one
+        oldest = alltweets[-1].id - 1
+        
+        print "...%s tweets downloaded so far" % (len(alltweets))
     
     #transform the tweepy tweets into a 2D array that will populate the csv 
-    outtweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in all_tweets]
-
-
+    outtweets = [[tweet.text.encode("utf-8")] for tweet in alltweets]
+    
     #write the csv  
-    with open('%s_tweets.csv' % screen_name, 'wb') as f:
+    with open('politicians/%s.csv' % screen_name, 'wb') as f:
         writer = csv.writer(f)
-        writer.writerow(["id","created_at","text"])
         writer.writerows(outtweets)
+    
     pass
 
 
 if __name__ == '__main__':
     #pass in the username of the account you want to download
     get_all_tweets("BarackObama")
+    get_all_tweets("BernieSanders")
+    get_all_tweets("realDonaldTrump")
+    get_all_tweets("SenWarren")
+    get_all_tweets("BetsyDeVosED")
 
