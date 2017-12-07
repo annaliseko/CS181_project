ccfd83ac83a04ff007b2f6dcd240160db9932ed3
Annalise Ko <annaliseliuko@gmail.com>
2017-12-06 17:21:48 -0800

    update

--- cleanTweets.py
+++ cleanTweets.py
@@ -6,17 +6,20 @@ stopwords = set(stopwords.words('english'))
 # added stopwords to nltk list
 stopwords.update(("another","rt", "here", 'go', "i've", "it's", "we've", "we'll", "you're",
 					"were","your","their","they're","there","aren't","didn't","that's",
-					"however","although","except"))
+					"however","although","except","great"))
 ## uncomment to list all stop words
-# print(stopwords)
+print(stopwords)
 
 # name of new file we want to write to
-new_file = open('text_clean.txt', 'w')
+new_file = open('politicians/files/clean/obama.txt', 'w')
 # open file to read
 with open("politicians/files/obama.txt","r") as f:
 	for line in f:
-		# remove twitter URLs
-		result = re.sub(r"http\S+", "", line)
+		# remove twitter URLs, hashtags, mentions
+		noURL = re.sub(r"http\S+", "", line)
+		noHashtag = re.sub(r'(\s)#\w+', r'\1', noURL)
+		noTags = re.sub(r'(\s)@\w+', r'\1', noHashtag)
+		result = re.sub(r'(\s).@\w+', r'\1', noTags)
 		stringTweet = result.split()
 		# remove stopwords
 		resultwords  = [word for word in stringTweet if word.lower() not in stopwords]
