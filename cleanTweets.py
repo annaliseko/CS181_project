761147a5a2555f44c6d5228b78fcef73dbee28bd
Patrick McDonough <31291053+pmcdonough35@users.noreply.github.com>
2017-12-07 01:00:53 -0800

    remove numbers

--- cleanTweets.py
+++ cleanTweets.py
@@ -1,30 +1,21 @@
-import nltk
-from nltk.corpus import stopwords
 import re
 
-stopwords = set(stopwords.words('english'))
-# added stopwords to nltk list
-stopwords.update(("another","rt", "here", 'go', "i've", "it's", "we've", "we'll", "you're",
-					"were","your","their","they're","there","aren't","didn't","that's",
-					"however","although","except","great"))
-## uncomment to list all stop words
-print(stopwords)
-
 # name of new file we want to write to
-new_file = open('politicians/files/clean/obama.txt', 'w')
+new_file = open('politicians/files/clean/warren.txt', 'w')
 # open file to read
-with open("politicians/files/obama.txt","r") as f:
+with open("politicians/files/warren.txt","r") as f:
 	for line in f:
-		# remove twitter URLs, hashtags, mentions
+		# remove twitter URLs
 		noURL = re.sub(r"http\S+", "", line)
-		noHashtag = re.sub(r'(\s)#\w+', r'\1', noURL)
-		noTags = re.sub(r'(\s)@\w+', r'\1', noHashtag)
-		result = re.sub(r'(\s).@\w+', r'\1', noTags)
-		stringTweet = result.split()
-		# remove stopwords
-		resultwords  = [word for word in stringTweet if word.lower() not in stopwords]
-		result = ' '.join(resultwords)
+		# remove numbers
+		noNum = re.sub(r'\d+', "", noURL)
+
+		## Uncomment below to also remove hashtags and mentions
+		# noHashtag = re.sub(r'(\s)#\w+', r'\1', noNum)
+		# noTags = re.sub(r'(\s)@\w+', r'\1', noHashtag)
+		# clean = re.sub(r'(\s).@\w+', r'\1', noTags)
+
 		# return new text in all lowercase letters
-		new_file.write(result.lower())
+		new_file.write(noNum.lower())
 
 new_file.close()   
